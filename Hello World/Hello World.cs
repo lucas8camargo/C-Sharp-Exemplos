cdc36c9b6bdfa1383504c1c7c019823552a35318
Lucas Camargo <lucas8camargo@outlook.com>
2021-07-02 22:36:33 -0300

    Hello, World!
    Um simples 'Hello, World!' no Console.

---
+++ Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Hello World/README.md
@@ -0,0 +1,3 @@
+# Sobre
+
+Um simples 'Hello, World!' no Console.
