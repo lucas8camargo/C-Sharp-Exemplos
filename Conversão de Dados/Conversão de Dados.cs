5c9acdc0f3304ffbc6f2bfa6a0e91fc451e0541a
Lucas Camargo <lucas8camargo@outlook.com>
2021-07-05 21:06:38 -0300

    Conversão de Dados

---
+++ Conversão de Dados/Conversão de Dados.cs
@@ -0,0 +1,13 @@
+var x;
+
+Convert.ToChar(X);
+Convert.ToInt16(X);
+Convert.ToInt32(X);
+Convert.ToInt64(X);
+Convert.ToString(X);
+Convert.ToBoolean(X);
+Convert.ToByte(X);
+Convert.ToDateTime(X);
+Convert.ToDecimal(X);
+Convert.ToDouble(X);
+Convert.ToSingle(X);
