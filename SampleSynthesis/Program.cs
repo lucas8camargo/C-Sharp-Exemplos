992c0e9c4e3b15fa9e8d35fe05e4b253ea969028
Lucas <lucas@email.com>
2022-03-25 16:29:55 -0300

    Exemplo do sintetizador de voz da biblioteca 'System.Speech'

---
+++ SampleSynthesis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace SampleSynthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();           
+
+            synth.Volume = 100; // Define o volume, 100 é o volume máximo.
+
+            synth.Rate = 2; // Define a velocidade da fala
+
+            Console.WriteLine("Falando...");
+
+            synth.Speak("Olá! Essa é uma amostra de voz");
+
+        }
+    }
+
+    // Adicione a biblioteca System.Speech
+
+    // Package Manager:
+    // Install-Package System.Speech
+    // .NET CLI:
+    // dotnet add package System.Speech
+}
