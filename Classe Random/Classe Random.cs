79267238f0ede6e1e96f3a2b3a1236d9b505c5fd
Lucas Camargo <lucasncamargo@outlook.com>
2022-04-15 22:28:53 -0300

    Update Classe Random.cs

--- Classe Random/Classe Random.cs
+++ Classe Random/Classe Random.cs
@@ -12,10 +12,6 @@ namespace classe_random
             int exemplo2 = n.Next(10);
             // Retorna um número de ponto flutuante aleatório maior ou igual a 0,0 e menor que 1.0.
             double exemplo3 = n.NextDouble();
-            // Resultados
-            Console.WriteLine($"Exemplo 1: {exemplo1}");
-            Console.WriteLine($"Exemplo 2: {exemplo2}");
-            Console.WriteLine($"Exemplo 3: {exemplo3}");
         }
     }
 }
